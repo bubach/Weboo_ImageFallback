6a912b4d893733d2f55d4043e8f6391b6bea0dc1
bubach <asmhacker@gmail.com>
2014-09-23 14:41:44 +0200

    Update Abstract.php

--- app/code/community/Weboo/ImageFallback/Model/Product/Image/Abstract.php
+++ app/code/community/Weboo/ImageFallback/Model/Product/Image/Abstract.php
@@ -7,7 +7,9 @@
  * Time: 18:44
  */
 
-if ('true' == (string)Mage::getConfig()->getNode('modules/Icommerce_CatalogImageFormat/active')) {
+if ('true' == (string)Mage::getConfig()->getNode('modules/Ridestore_ImageCache/active')) {
+    class Weboo_ImageFallback_Model_Product_Image_Abstract extends Ridestore_ImageCache_Model_Product_Image {}
+} else if ('true' == (string)Mage::getConfig()->getNode('modules/Icommerce_CatalogImageFormat/active')) {
     class Weboo_ImageFallback_Model_Product_Image_Abstract extends Icommerce_CatalogImageFormat_Model_Product_Image {}
 } else {
     class Weboo_ImageFallback_Model_Product_Image_Abstract extends Mage_Catalog_Model_Product_Image {}
